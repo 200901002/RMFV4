1b66211b5670963ba0a66ccb948ab71a86ba2280
Adhithya03 <trainadhi03@gmail.com>
2023-10-14 13:00:19 +0530

    Wokring code tested on 13-10-2023

--- RMFV4.ino
+++ RMFV4.ino
@@ -3,54 +3,55 @@
 #define buringConstant 1
 
 #define LED_PIN    7
-#define LED_COUNT 10
+#define LED_COUNT 54
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 int j;
 
-
-//Frequency
-const int RphaseIntPin = 2;
-float frequency = 15;
+const int RphaseIntPin = 3;
+float frequency = 0;
 volatile unsigned long startTime = 0;
 volatile unsigned long endTime = 0;
 volatile bool risingEdge = false;
-volatile bool measureFrequency = false;
-const float minFrequency = 0.0;
-const float maxFrequency = 50.0;
+volatile bool measureFrequency = true;
+const float minFrequency = 1.1;
+const float maxFrequency = 50;
 
 void setup() {
   pinMode(RphaseIntPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(RphaseIntPin), handleInterrupt, CHANGE);
   strip.begin();       
   strip.show();
-  strip.setBrightness(150);
+  strip.setBrightness(255);
   Serial.begin(9600);
 }
 
 void loop() {
-  frequency = 55;
-  // measureFrequency = true;
-  // delay(600);
-  calcuatingFreqPattern(5);
+
   // measureFrequency = false;
-  
+  // delay(100);
+  measureFrequency = true; 
   if (isValidFrequency(frequency)) {
-    Serial.print("Frequency: ");
     Serial.println(frequency);
-    float timePeriod = (1 / frequency) * 1000;  // Very important.
-    NorthSouthChasing(timePeriod/buringConstant);
-    // delay(timePeriod / 4);                     // Flux - voltage delay.
-    // delay(timePeriod / 4);                     // wait for R peak.  
+    float timePeriod = (1 / frequency) * 1000;
+    for(int i=0;i<10;i++){
+    NorthSouthChasing(timePeriod/LED_COUNT);
+
+    }
+    delay(timePeriod / (LED_COUNT*4));// Flux - voltage delay.
+    delay(timePeriod / (LED_COUNT*4));// wait for R peak.  
   }
-}
+   }
+
 
 bool isValidFrequency(float freq) {
   return freq >= minFrequency && freq <= maxFrequency;
-}       
+}  
+
+
 void handleInterrupt() {
-  delay(100);
+  delayMicroseconds(100);
   if (measureFrequency) {
     int sensorValue = digitalRead(RphaseIntPin);
 
@@ -65,21 +66,17 @@ void handleInterrupt() {
     }
   }
 }
+
+
 void NorthSouthChasing(float wait) {
   for (int i = 0; i < strip.numPixels(); i++) {
-    int redIndex = i; // Offset for the red light
-    int blueIndex = (i + 5) % strip.numPixels(); // Offset for the blue light with a fixed offset of 4
+    int redIndex = i; 
+    int blueIndex = (i + (LED_COUNT/2)) % strip.numPixels();
     strip.clear();
-    strip.setPixelColor(redIndex, strip.Color(255, 1,5)); // Set the main red pixel
-    strip.setPixelColor(blueIndex, strip.Color(5, 1,255)); // Set the main blue pixel
+    strip.setPixelColor(redIndex, strip.Color(255, 0,0)); // Set the main red pixel
+    strip.setPixelColor(blueIndex, strip.Color(0, 0, 255)); // Set the main blue pixel
     strip.show();
     delay(wait);
-  }
-}
-void calcuatingFreqPattern(int wait) {
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    strip.rainbow(firstPixelHue);
-    strip.show(); 
-    delay(wait);
+  
   }
 }
